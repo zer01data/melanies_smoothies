da4d817dcfe7d186a8548be57b11c588fcb2a0d6
zer01data <zer01data@icloud.com>
2025-10-23 12:47:59 -0700

    Disable Snowflake session connection
    Comment out the Snowflake session import and initialization.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,7 +10,11 @@ st.write(
   """
 )
 
-session = get_active_session()
+
+# session = get_active_session()
+cnx= st.connect("Snowflake")
+session = cnx.session()
+
 name_on_order = st.text_input("Name on Smoothie:" )
 
 #option = st.selectbox(
